22af149f5590c531064a0e415c13b4747e55aa91
Christian Ayala <christian@dialexa.com>
2018-01-27 09:57:18 -0600

    Renamed folders, added 01/26 code

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,10 @@ module.exports = {
     },
     "rules": {
         "no-const-assign": 2,
-        "linebreak-style": 0
+        "linebreak-style": 0,
+        "valid-jsdoc": 0,
+        "require-jsdoc": 0,
+        "quotes": 0,
+        "max-len": 0
     }
 };
